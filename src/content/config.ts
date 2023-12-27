b124aaea79362c2884c440754a3ea4f82b783082
Kahvi Patel <iamkahvi@gmail.com>
2023-12-27 10:59:09 -0500

    collections are really really great

---
+++ src/content/config.ts
@@ -0,0 +1,32 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    date: z.date(),
+  }),
+});
+
+const highlightsCollection = defineCollection({
+  type: "data", // v2.5.0 and later
+  schema: z.array(
+    z.object({
+      book: z.string(),
+      author: z.string(),
+      quote: z.string(),
+      location: z.object({
+        start: z.number().nullable(),
+        end: z.number().nullable(),
+      }),
+      dateAdded: z.number(),
+    }),
+  ),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  "posts": blogCollection,
+  "highlights": highlightsCollection,
+};
